f06b136ea15f62be79638a4b890e2ab95412c8fe
Tadpul <david.bn.nartey@gmail.com>
2026-04-03 02:17:49 +0100

    FINALLY GOT SDL3 WORKING OMG

---
+++ test/main.cpp
@@ -0,0 +1,16 @@
+#include <SDL3/SDL.h>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+        std::cout << "1";
+        return -1;
+    }
+    
+    SDL_Window* window = SDL_CreateWindow("Hello SDL3", 640, 480, 0);
+    SDL_Delay(3000); // Wait 3 seconds
+    
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+    return 0;
+}
