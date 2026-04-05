d1b8a97c37bb53601f4f9c0c945f469b3b09851b
Tadpul <david.bn.nartey@gmail.com>
2026-04-05 01:48:32 +0100

    learning sdl

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,35 @@
 #include <SDL3/SDL.h>
 #include <iostream>
+#include <vector>
 #include "Matrix.h"
 #include "Vector.h"
 
-int main(int argc, char* argv[]) {
-    if (!SDL_Init(SDL_INIT_VIDEO)) {
-        return -1;
+int main()
+{
+    if (!SDL_Init(SDL_INIT_VIDEO)) return SDL_APP_FAILURE;
+
+    SDL_Window* window = SDL_CreateWindow("my window", 2000, 1000, 0);
+    SDL_Event event;
+
+    const bool* keys = SDL_GetKeyboardState(nullptr);
+
+    // application runs until exit
+    bool running{ true };
+    while (running)
+    {
+        // event handling loop
+        while (SDL_PollEvent(&event))
+        {
+            if (event.type == SDL_EVENT_QUIT) running = false;
+            else if (event.type == SDL_EVENT_KEY_DOWN) 
+            {
+                SDL_Log("a key was pressed: %d", event.key.scancode);
+            }
+        }
+
+        if (keys[SDL_SCANCODE_P] == true) SDL_Log("letter p was pressed");
     }
-    
-    SDL_Window* window = SDL_CreateWindow("Hello SDL3", 640, 480, 0);
-    SDL_Delay(3000); // Wait 3 seconds
-    
-    SDL_DestroyWindow(window);
+
     SDL_Quit();
     return 0;
 }
