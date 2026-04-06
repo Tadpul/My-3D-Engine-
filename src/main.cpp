a2453cd5de1051454958ee39ca7749c0b83b03d5
Tadpul <david.bn.nartey@gmail.com>
2026-04-06 03:03:06 +0100

    converted sdl coordinates to world coordinates and vice versa

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,27 @@
 #include <SDL3/SDL.h>
 #include <iostream>
 #include <vector>
+#include <SDL3/SDL_render.h>
 #include "Matrix.h"
 #include "Vector.h"
+
+std::array<float, 2> screenToWorldTransform(const float &xScreenSize, const float &yScreenSize, float &xSDL, float &ySDL)
+{
+    const float displacementFactor{ 1 / std::min(xScreenSize, yScreenSize) };
+    const float x = 2 * (xSDL - 0.5*xScreenSize) * displacementFactor;
+    const float y = -2 * (ySDL + 0.5*yScreenSize) * displacementFactor + 2;
+    return std::array<float, 2>{x, y};
+}
 
 int main()
 {
     if (!SDL_Init(SDL_INIT_VIDEO)) return SDL_APP_FAILURE;
 
-    SDL_Window* window = SDL_CreateWindow("my window", 2000, 1000, 0);
-    SDL_Event event;
+    constexpr float xScreenSize{ 2000 };
+    constexpr float yScreenSize{ 1000 };
+    SDL_Window* window = SDL_CreateWindow("my window", xScreenSize, yScreenSize, 0);
+    SDL_Renderer* renderer;
+    SDL_Event event;    
 
     const bool* keys = SDL_GetKeyboardState(nullptr);
 
@@ -24,6 +36,11 @@ int main()
             else if (event.type == SDL_EVENT_KEY_DOWN) 
             {
                 SDL_Log("a key was pressed: %d", event.key.scancode);
+            }
+            else if (event.type == SDL_EVENT_MOUSE_MOTION)
+            {
+                std::array<float, 2> worldCoordinates{ screenToWorldTransform(xScreenSize, yScreenSize, event.motion.x, event.motion.y) };
+                SDL_Log("x, y: %f, %f", worldCoordinates[0], worldCoordinates[1]);
             }
         }
 
